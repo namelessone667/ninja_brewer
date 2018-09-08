d3c88062d23c26843f0cf516bf4ff1d27fac5962
namelessone667 <35223067+namelessone667@users.noreply.github.com>
2018-09-08 16:53:59 +0200

    model refactor

--- src/NinjaModel.h
+++ src/NinjaModel.h
@@ -7,7 +7,7 @@
 #include "enum.h"
 #include "globals.h"
 #include <map>
-//#include <variant>
+#include "variant.h"
 //#include "boost/variant.hpp"
 
 //define a key for every Property
@@ -53,22 +53,19 @@ enum NinjaModelPropertyKey
   KEY_EXTERNALTEMPPROFILEACTIVE
 };
 
-template<typename T, NinjaModelPropertyKey key>
+template<typename T>
 class NinjaModelProperty : public Property<T>
 {
 public:
-  NinjaModelProperty()
-  {
-    Property<T>();
-  }
+  NinjaModelProperty(NinjaModelPropertyKey key) : Property<T>(), _key(key)  {  };
 
-  NinjaModelProperty(const T value) : Property<T>(value) { };
+  NinjaModelProperty(NinjaModelPropertyKey key, const T value) : Property<T>(value), _key(key) { };
 
-  NinjaModelProperty(const Property<T>& value) : Property<T>(value.Get()) { };
+  NinjaModelProperty(NinjaModelPropertyKey key, const Property<T>& value) : Property<T>(value.Get()), _key(key) { };
 
-  NinjaModelProperty(const NinjaModelProperty& value) : Property<T>(value.Get()) { };
+  NinjaModelProperty(const NinjaModelProperty& value) : Property<T>(value.Get()), _key(value.getKey()) { };
 
-  NinjaModelPropertyKey getKey() const { return key; };
+  NinjaModelPropertyKey getKey() const { return _key; };
 
   NinjaModelProperty& operator=(const T& other)
   {
@@ -81,56 +78,58 @@ public:
     Property<T>::operator= (other);
     return *this;
   }
+private:
+  NinjaModelPropertyKey _key;
 };
 
 class NinjaModel
 {
-  // typedef std::variant< NinjaModelProperty<double, NinjaModelPropertyKey>,
-  //                       NinjaModelProperty<int, NinjaModelPropertyKey>,
-  //                       NinjaModelProperty<bool, NinjaModelPropertyKey>,
-  //                       NinjaModelProperty<opMode, NinjaModelPropertyKey>,
-  //                       NinjaModelProperty<opState, NinjaModelPropertyKey>,
-  //                       NinjaModelProperty<ApplicationState, NinjaModelPropertyKey>> NinjaModelPropertyVariant;
+   typedef nonstd::variant< NinjaModelProperty<double>,
+                         NinjaModelProperty<int>,
+                         NinjaModelProperty<bool>,
+                         NinjaModelProperty<opMode>,
+                         NinjaModelProperty<opState>,
+                         NinjaModelProperty<ApplicationState>> NinjaModelPropertyVariant;
 public:
-    NinjaModelProperty<double, KEY_SETPOINT> SetPoint; // target beer temp
-    NinjaModelProperty<double, KEY_OUTPUT> Output; // main PID output temperature, also setpoint for heatPID
-    NinjaModelProperty<double, KEY_PEAKESTIMATOR> PeakEstimator;
-    NinjaModelProperty<double, KEY_PIDKP> PID_Kp;
-    NinjaModelProperty<double, KEY_PIDKI> PID_Ki;
-    NinjaModelProperty<double, KEY_PIDKD> PID_Kd;
-    NinjaModelProperty<int, KEY_PIDMODE> PIDMode;
-    NinjaModelProperty<double, KEY_HEATOUTPUT> HeatOutput;
-    NinjaModelProperty<double, KEY_HEATPIDKP> HeatPID_Kp;
-    NinjaModelProperty<double, KEY_HEATPIDKI> HeatPID_Ki;
-    NinjaModelProperty<double, KEY_HEATPIDKD> HeatPID_Kd;
-    NinjaModelProperty<int, KEY_HEATPIDMODE> HeatPIDMode;
-    NinjaModelProperty<bool, KEY_STANDBY> StandBy;
+    NinjaModelProperty<double> SetPoint; // target beer temp
+    NinjaModelProperty<double> Output; // main PID output temperature, also setpoint for heatPID
+    NinjaModelProperty<double> PeakEstimator;
+    NinjaModelProperty<double> PID_Kp;
+    NinjaModelProperty<double> PID_Ki;
+    NinjaModelProperty<double> PID_Kd;
+    NinjaModelProperty<int> PIDMode;
+    NinjaModelProperty<double> HeatOutput;
+    NinjaModelProperty<double> HeatPID_Kp;
+    NinjaModelProperty<double> HeatPID_Ki;
+    NinjaModelProperty<double> HeatPID_Kd;
+    NinjaModelProperty<int> HeatPIDMode;
+    NinjaModelProperty<bool> StandBy;
 
-    NinjaModelProperty<double, KEY_IDLEDIFF> IdleDiff;          // constrain fridge temperature to +/- 0.5 deg C (0.9 deg F) differential
-    NinjaModelProperty<double, KEY_PEAKDIFF> PeakDiff;          // constrain allowed peak error to +/- 0.25 deg C (0.45 deg F) differential
-    NinjaModelProperty<int, KEY_COOLMINOFF> CoolMinOff;  // minimum compressor off time, seconds (5 min)
-    NinjaModelProperty<int, KEY_COOLMINON> CoolMinOn;   // minimum compressor on time, seconds (1.5 min)
-    NinjaModelProperty<int, KEY_COOLMAXON> CoolMaxOn;   // maximum compressor on time, seconds (45 min)
-    NinjaModelProperty<int, KEY_PEAKMAXTIME> PeakMaxTime; // maximum runTime to consider for peak estimation, seconds (20 min)
-    NinjaModelProperty<int, KEY_PEAKMAXWAIT> PeakMaxWait; // maximum wait on peak, seconds (30 min)
-    NinjaModelProperty<int, KEY_HEATMINOFF> HeatMinOff;  // minimum HEAT off time, seconds (5 min)
-    NinjaModelProperty<int, KEY_HEATWINDOW> HeatWindow; // window size for HEAT time proportioning, ms (5 min)
-    NinjaModelProperty<int, KEY_MINIDLETIME> MinIdleTime;// minimum idle time between cool -> heat or heat -> cool
-    NinjaModelProperty<double, KEY_NOHEATBELOW> NoHeatBelow;     // dont turn on heating when tempretare is bellow this
-    NinjaModelProperty<double, KEY_NOCOOLABOVE> NoCoolAbove;     // dont turn on cooling when tempretare is abowe this
-    NinjaModelProperty<opMode, KEY_CONTROLLERMODE> ControllerMode;
-    NinjaModelProperty<double, KEY_MINTEMP> MinTemperature;
-    NinjaModelProperty<double, KEY_MAXTEMP> MaxTemperature;
-    NinjaModelProperty<double, KEY_HEATMINPERCENT> HeatMinPercent;
-    NinjaModelProperty<double, KEY_HEATMAXPERCENT> HeatMaxPercent;
-    NinjaModelProperty<double, KEY_HEATMANUALOUTPUT> HeatManualOutputPercent;
+    NinjaModelProperty<double> IdleDiff;          // constrain fridge temperature to +/- 0.5 deg C (0.9 deg F) differential
+    NinjaModelProperty<double> PeakDiff;          // constrain allowed peak error to +/- 0.25 deg C (0.45 deg F) differential
+    NinjaModelProperty<int> CoolMinOff;  // minimum compressor off time, seconds (5 min)
+    NinjaModelProperty<int> CoolMinOn;   // minimum compressor on time, seconds (1.5 min)
+    NinjaModelProperty<int> CoolMaxOn;   // maximum compressor on time, seconds (45 min)
+    NinjaModelProperty<int> PeakMaxTime; // maximum runTime to consider for peak estimation, seconds (20 min)
+    NinjaModelProperty<int> PeakMaxWait; // maximum wait on peak, seconds (30 min)
+    NinjaModelProperty<int> HeatMinOff;  // minimum HEAT off time, seconds (5 min)
+    NinjaModelProperty<int> HeatWindow; // window size for HEAT time proportioning, ms (5 min)
+    NinjaModelProperty<int> MinIdleTime;// minimum idle time between cool -> heat or heat -> cool
+    NinjaModelProperty<double> NoHeatBelow;     // dont turn on heating when tempretare is bellow this
+    NinjaModelProperty<double> NoCoolAbove;     // dont turn on cooling when tempretare is abowe this
+    NinjaModelProperty<opMode> ControllerMode;
+    NinjaModelProperty<double> MinTemperature;
+    NinjaModelProperty<double> MaxTemperature;
+    NinjaModelProperty<double> HeatMinPercent;
+    NinjaModelProperty<double> HeatMaxPercent;
+    NinjaModelProperty<double> HeatManualOutputPercent;
 
-    NinjaModelProperty<double, KEY_FRIDGETEMP> FridgeTemp = -127.0; //also input for heatPID
-    NinjaModelProperty<double, KEY_BEERTEMP> BeerTemp = -127.0; //also Input for mainPID
-    NinjaModelProperty<opState, KEY_CONTROLLERSTATE> ControllerState = IDLE;
-    NinjaModelProperty<ApplicationState, KEY_APPSTATE> AppState = UNDEFINED;
+    NinjaModelProperty<double> FridgeTemp = -127.0; //also input for heatPID
+    NinjaModelProperty<double> BeerTemp = -127.0; //also Input for mainPID
+    NinjaModelProperty<opState> ControllerState = IDLE;
+    NinjaModelProperty<ApplicationState> AppState = UNDEFINED;
 
-    NinjaModelProperty<bool, KEY_EXTERNALTEMPPROFILEACTIVE> ExternalProfileActive; // true if Fermentrack temperature profile is active
+    NinjaModelProperty<bool> ExternalProfileActive; // true if Fermentrack temperature profile is active
 
 private:
     //TODO: include boost library or implement a ligthweigth version of boost:variant type
